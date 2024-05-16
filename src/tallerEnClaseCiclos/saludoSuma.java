0de1f4199a9dbca16a697cdd09ee4efdc2b44193
Julianmancipe10 <148405224+Julianmancipe10@users.noreply.github.com>
2024-05-15 22:23:02 -0500

    Update saludoSuma.java

--- src/tallerEnClaseCiclos/saludoSuma.java
+++ src/tallerEnClaseCiclos/saludoSuma.java
@@ -5,19 +5,19 @@ public class saludoSuma {
 	public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
-        // Pedir la cantidad de veces que se repetirá el proceso
+    
         System.out.println("Ingrese la cantidad de veces que desea realizar el proceso: ");
         int veces = scanner.nextInt();
-        scanner.nextLine(); // Consumir el salto de línea
+        scanner.nextLine(); 
         
-        // Ciclo for para repetir el proceso 'veces' veces
+      
         for (int i = 0; i < veces; i++) {
-            // Pedir al usuario que ingrese el código
+           
             System.out.println("Ingrese el código (1 para saludar, 2 para pedir nombre, 3 para sumar dos números): ");
             int codigo = scanner.nextInt();
-            scanner.nextLine(); // Consumir el salto de línea
+            scanner.nextLine();
             
-            // Realizar la acción correspondiente según el código ingresado
+          
             switch (codigo) {
                 case 1:
                     System.out.println("¡Hola! Bienvenido.");
